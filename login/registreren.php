e88668351a809efc0bd7915d9c9d6c101ab33594
Julian <115456102+MonkeMK@users.noreply.github.com>
2024-02-05 10:58:07 +0100

    back-end login

---
+++ login/registreren.php
@@ -0,0 +1,34 @@
+<?php
+include("registreren.html");
+inlcude("bibliotheek/mailen.php");
+inlcude(DBconfig.php);
+if(isset($_POST["submit"]))
+{
+    $melding=" ";
+    $voornaam=htmlspecialchars($_POST[voornaam]);
+    $achternaam=htmlspecialchars($_POST[achternaam]);
+    $klant=$voornaam . " " .$achternaam;
+    $straat=htmlspecialchars($_POST[straat]);
+    $postcode=htmlspecialchars($POST[postcode]);
+    $woonplaats=htmlspecialchars($POST[vwoonplaats]);
+    $email=htmlspecialchars($POST[email]);
+    $wachtwoord=htmlspecialchars($POST[wachtwoord]);
+    $wachtwoordhash=password_hash($wachtwoord, PASSWORD_DEFAULT);
+}
+
+// duplicaat email checker
+$sql = "SELECT * FROM klant WHERE email = ?";
+$stmt = @verbinding->prepare($sql);
+$stmt->execute(array($email));
+$resultaat = $stmt->fetch(PDO: :FETCH_ASSOC);
+if ($resultaat) {
+    $melding="Deze email staat al in het systeem";
+}else{
+    $sql = "INSERT INTO klant (ID, voornaam achternaam
+    straat, postcode, woonplaats, email,
+    wachtwoord, rol) values (null,?,?,?,?,?,?,?,?,)";
+    $stmt=$verbinding->prepare($sql);
+    try{
+        
+    }
+}
