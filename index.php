21e01afedb5d945f75c3d95bf47aa54ec50f5334
MonkeMK <julian.verboom8@gmail.com>
2024-02-28 10:07:20 +0100

    fixed hoop ik

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
     
     include_once("header.php");
     include_once('database.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
