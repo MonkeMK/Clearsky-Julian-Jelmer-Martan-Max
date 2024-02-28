45439215e625c6ec7b753735f0e3cadeca412dc3
MonkeMK <julian.verboom8@gmail.com>
2024-02-28 12:56:07 +0100

    still broken

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>redirect</title>
+    <meta http-equiv="refresh" content="0;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+</head>
+<body>
+</body>
+</html>
+
+
+
+
 <?php
 include("registreren.html");
 inlcude("bibliotheek/mailen.php");
