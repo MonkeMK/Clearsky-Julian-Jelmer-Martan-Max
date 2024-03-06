fc172271b589dcb84d798469b22200d8ce7327c8
Jelmer420 <118176732+Jelmer420@users.noreply.github.com>
2024-03-06 10:25:01 +0100

    agag

--- checkout.php
+++ checkout.php
@@ -14,14 +14,14 @@ include_once("database.php");
 <style>
     .container {
         position: absolute;
-        top:60%;
+        top: 60%;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
     }
 
-    .form{
+    .form {
         position: absolute;
         left: 10%;
         top: 20%;
@@ -57,7 +57,8 @@ include_once("database.php");
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
         grid-gap: 10px;
-        margin-top: 4%; /* Adjust the top margin to move the cards down */
+        margin-top: 4%;
+        /* Adjust the top margin to move the cards down */
         margin-left: 5%;
     }
 </style>
@@ -86,14 +87,14 @@ include_once("database.php");
                     ?>
 
                     <div class="card">
-                        <img src="assets/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>" class="img-fluid m-4"
-                            style="max-width: 200px; max-height: 200px;">
+                        <img src="assets/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>"
+                            class="img-fluid m-4" style="max-width: 200px; max-height: 200px;">
                         <div class="card-body">
                             <h5 class="card-title">
                                 <?php echo $product["name"]; ?>
                             </h5>
                             <?php echo "Beschrijving: " . $product["description"]; ?><br>
-                            <?php echo "Prijs: €" . number_format($price * $item["quantity"], 2); ?><br>
+                            <?php echo "Prijs: €" . number_format($product["price"] * $item["quantity"], 2); ?><br>
                             <?php echo "Aantal: " . $item['quantity']; ?><br>
                             <br>
                             <a href="remove.php?key=<?php echo $key; ?>" class="btn btn-danger">Remove</a>
@@ -103,6 +104,7 @@ include_once("database.php");
             } else {
                 echo "<p>You have not added any pizzas.</p>";
             }
+
             ?>
         </div>
     </div>
@@ -116,31 +118,89 @@ include_once("database.php");
 <hr style="position:absolute; top:67%; width:95%; border-top: 2px solid black; left:2.5%;">
 <br>
 <be>
-<div class="container">
-    <div class="offcanvas-body">
-        <div class="center-content">
-            <div class="w-50">
-                <h3 class="titel">Factuurgegevens</h3>
-                <div class="form">
-                    <form action="checkout.php" method="POST" style="display: flex; flex-direction: column; align-items: center;">
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="address" class="form-label">Address:</label>
-                            <input type="text" class="form-control" id="address" name="address" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="zipcode" class="form-label">Zip code:</label>
-                            <input type="text" class="form-control" id="zipcode" name="zipcode" required>
-                        </div>
-                        <button type="submit" name="checkout" value="1" class="btn btn-primary" <?php if (empty($cart->getCart())) {
-                            echo "disabled";
-                        } ?>>Checkout</button>
-                    </form>
+    <div class="container">
+        <div class="offcanvas-body">
+            <div class="center-content">
+                <div class="w-50">
+                    <h3 class="titel">Factuurgegevens</h3>
+                    <div class="form">
+                        <form action="checkout.php" method="POST"
+                            style="display: flex; flex-direction: column; align-items: center;">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email:</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="land" class="form-label">Land:</label>
+                                <select class="form-select" id="country" name="country">
+                                    <option value="">Select your country</option>
+                                    <option value="AX">Aland Islands</option>
+                                    <option value="AL">Albania</option>
+                                    <option value="AD">Andorra</option>
+                                    <option value="AT">Austria</option>
+                                    <option value="BY">Belarus</option>
+                                    <option value="BE">Belgium</option>
+                                    <option value="BA">Bosnia and Herzegovina</option>
+                                    <option value="BG">Bulgaria</option>
+                                    <option value="HR">Croatia</option>
+                                    <option value="CZ">Czech Republic</option>
+                                    <option value="DK">Denmark</option>
+                                    <option value="EE">Estonia</option>
+                                    <option value="FO">Faroe Islands</option>
+                                    <option value="FI">Finland</option>
+                                    <option value="FR">France</option>
+                                    <option value="DE">Germany</option>
+                                    <option value="GI">Gibraltar</option>
+                                    <option value="GR">Greece</option>
+                                    <option value="GG">Guernsey</option>
+                                    <option value="VA">Holy See (Vatican City State)</option>
+                                    <option value="HU">Hungary</option>
+                                    <option value="IS">Iceland</option>
+                                    <option value="IE">Ireland</option>
+                                    <option value="IM">Isle of Man</option>
+                                    <option value="IT">Italy</option>
+                                    <option value="JE">Jersey</option>
+                                    <option value="XK">Kosovo</option>
+                                    <option value="LV">Latvia</option>
+                                    <option value="LI">Liechtenstein</option>
+                                    <option value="LT">Lithuania</option>
+                                    <option value="LU">Luxembourg</option>
+                                    <option value="MK">Macedonia, the Former Yugoslav Republic of</option>
+                                    <option value="MT">Malta</option>
+                                    <option value="MD">Moldova, Republic of</option>
+                                    <option value="MC">Monaco</option>
+                                    <option value="ME">Montenegro</option>
+                                    <option value="NL">Netherlands</option>
+                                    <option value="NO">Norway</option>
+                                    <option value="PL">Poland</option>
+                                    <option value="PT">Portugal</option>
+                                    <option value="RO">Romania</option>
+                                    <option value="SM">San Marino</option>
+                                    <option value="RS">Serbia</option>
+                                    <option value="CS">Serbia and Montenegro</option>
+                                    <option value="SK">Slovakia</option>
+                                    <option value="SI">Slovenia</option>
+                                    <option value="ES">Spain</option>
+                                    <option value="SJ">Svalbard and Jan Mayen</option>
+                                    <option value="SE">Sweden</option>
+                                    <option value="CH">Switzerland</option>
+                                    <option value="UA">Ukraine</option>
+                                    <option value="GB">United Kingdom</option>
+                                </select>
+                                <div class="mb-3">
+                                    <label for="address" class="form-label">Address:</label>
+                                    <input type="text" class="form-control" id="address" name="address" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="zipcode" class="form-label">Zip code:</label>
+                                    <input type="text" class="form-control" id="zipcode" name="zipcode" required>
+                                </div>
+                                <button type="submit" name="checkout" value="1" class="btn btn-primary" <?php if (empty($cart->getCart())) {
+                                    echo "disabled";
+                                } ?>>Checkout</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
