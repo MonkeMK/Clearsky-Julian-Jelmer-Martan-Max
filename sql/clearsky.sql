e27b71875cf28fc0d0b53a80ea5d93ab33f2a4d0
Jelmer420 <118176732+Jelmer420@users.noreply.github.com>
2024-04-10 08:53:35 +0200

    afaf

--- sql/clearsky.sql
+++ sql/clearsky.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2024 at 09:41 AM
+-- Generation Time: Apr 10, 2024 at 08:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
